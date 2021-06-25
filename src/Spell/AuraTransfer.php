541557ff8ea288d76b8af21b598136da8527965e
lemuria-pbem <lemuria@online.de>
2021-06-25 07:45:02 +0200

    Add some more simple spells.

---
+++ src/Spell/AuraTransfer.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types = 1);
+namespace Lemuria\Model\Fantasya\Spell;
+
+final class AuraTransfer extends AbstractSpell
+{
+	private const AURA = 2;
+
+	private const DIFFICULTY = 4;
+
+	public function Aura(): int {
+		return self::AURA;
+	}
+
+	public function Difficulty(): int {
+		return self::DIFFICULTY;
+	}
+}
