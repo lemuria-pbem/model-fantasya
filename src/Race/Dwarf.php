ba3e17cae8c1bcca06c53605fb971eac3f0b5a7b
lemuria-pbem <lemuria@online.de>
2021-11-13 18:22:01 +0100

    Give dwarves alchemy bonus.

--- src/Race/Dwarf.php
+++ src/Race/Dwarf.php
@@ -7,6 +7,7 @@ use JetBrains\PhpStorm\Pure;
 use Lemuria\Model\Fantasya\Landscape\Glacier;
 use Lemuria\Model\Fantasya\Landscape\Mountain;
 use Lemuria\Model\Fantasya\Modification;
+use Lemuria\Model\Fantasya\Talent\Alchemy;
 use Lemuria\Model\Fantasya\Talent\Archery;
 use Lemuria\Model\Fantasya\Talent\Armory;
 use Lemuria\Model\Fantasya\Talent\Bladefighting;
@@ -73,13 +74,13 @@ final class Dwarf extends AbstractRace
 
 	#[Pure] protected function mods(): array {
 		return [
-			Archery::class       => -1, Armory::class        =>  2, Bladefighting::class =>  1,
-			Camouflage::class    => -1, Catapulting::class   =>  2, Constructing::class  =>  2,
-			Entertaining::class  => -1, Herballore::class    => -2, Horsetaming::class   => -2,
-			Magic::class         => -2, Mining::class        =>  2, Navigation::class    => -2,
-			Quarrying::class     =>  2, Riding::class        => -2, Roadmaking::class    =>  2,
-			Shipbuilding::class  => -1, Taxcollecting::class =>  1, Trading::class       =>  1,
-			Weaponry::class      =>  2, Woodchopping::class  => -1
+			Alchemy::class       =>  2, Archery::class       => -1, Armory::class        =>  2,
+			Bladefighting::class =>  1, Camouflage::class    => -1, Catapulting::class   =>  2,
+			Constructing::class  =>  2, Entertaining::class  => -1, Herballore::class    => -2,
+			Horsetaming::class   => -2, Magic::class         => -2, Mining::class        =>  2,
+			Navigation::class    => -2, Quarrying::class     =>  2, Riding::class        => -2,
+			Roadmaking::class    =>  2, Shipbuilding::class  => -1, Taxcollecting::class =>  1,
+			Trading::class       =>  1, Weaponry::class      =>  2, Woodchopping::class  => -1
 		];
 	}
 
