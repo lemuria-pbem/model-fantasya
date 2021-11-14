39cc2c3b5e3bd4661f046bea3a2bec433024c9c6
lemuria-pbem <lemuria@online.de>
2021-11-14 16:21:03 +0100

    Make Ghouls swamp creatures.

--- src/Commodity/Monster/Ghoul.php
+++ src/Commodity/Monster/Ghoul.php
@@ -4,9 +4,6 @@ namespace Lemuria\Model\Fantasya\Commodity\Monster;
 
 use JetBrains\PhpStorm\Pure;
 
-use Lemuria\Model\Fantasya\Landscape\Forest;
-use Lemuria\Model\Fantasya\Landscape\Glacier;
-use Lemuria\Model\Fantasya\Landscape\Plain;
 use Lemuria\Model\Fantasya\Landscape\Swamp;
 
 final class Ghoul extends AbstractMonster
@@ -19,9 +16,6 @@ final class Ghoul extends AbstractMonster
 
 	public function __construct() {
 		$this->environment[] = self::createLandscape(Swamp::class);
-		$this->environment[] = self::createLandscape(Forest::class);
-		$this->environment[] = self::createLandscape(Plain::class);
-		$this->environment[] = self::createLandscape(Glacier::class);
 	}
 
 	#[Pure] public function Hitpoints(): int {
