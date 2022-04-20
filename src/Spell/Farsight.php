109579420593da0dcca9082c1ad2e320652ebef2
lemuria-pbem <lemuria@online.de>
2022-04-20 11:22:21 +0200

    Add new far sight spell.

---
+++ src/Spell/Farsight.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types = 1);
+namespace Lemuria\Model\Fantasya\Spell;
+
+final class Farsight extends AbstractSpell
+{
+	private const AURA = 3;
+
+	private const DIFFICULTY = 3;
+
+	protected bool $isIncremental = false;
+
+	public function Aura(): int {
+		return self::AURA;
+	}
+
+	public function Difficulty(): int {
+		return self::DIFFICULTY;
+	}
+}
