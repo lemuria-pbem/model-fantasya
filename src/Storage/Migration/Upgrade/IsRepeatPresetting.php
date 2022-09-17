e916d46c5b91d5d4caad7eb7e5d80ef0c32da0db
lemuria-pbem <lemuria@online.de>
2022-09-17 13:52:58 +0200

    [FEATURE] Add migration to add isRepeat presetting.

---
+++ src/Storage/Migration/Upgrade/IsRepeatPresetting.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types = 1);
+namespace Lemuria\Model\Fantasya\Storage\Migration\Upgrade;
+
+use Lemuria\Model\Fantasya\Storage\Migration\AbstractUpgrade;
+
+class IsRepeatPresetting extends AbstractUpgrade
+{
+	protected string $before = '1.0.0';
+
+	protected string $after = '1.1.0';
+
+	public function upgrade(): AbstractUpgrade {
+		$parties = $this->game->getParties();
+		foreach ($parties as $p => $party) {
+			if (!array_key_exists('isRepeat', $party['presettings'])) {
+				$parties[$p]['presettings']['isRepeat'] = false;
+			}
+		}
+		$this->game->setParties($parties);
+		return $this->finish();
+	}
+}
