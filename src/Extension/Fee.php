2bc2dde60cf264c3bd4b7383a8c1ab29d82bd563
lemuria-pbem <lemuria@online.de>
2022-12-30 08:57:30 +0100

    [TASK] Refactor a Fee extension out of the Market.

---
+++ src/Extension/Fee.php
@@ -0,0 +1,66 @@
+<?php
+declare(strict_types = 1);
+namespace Lemuria\Model\Fantasya\Extension;
+
+use Lemuria\Exception\UnserializeException;
+use Lemuria\Model\Fantasya\Extension;
+use Lemuria\Model\Fantasya\Factory\BuilderTrait;
+use Lemuria\Model\Fantasya\Quantity;
+use Lemuria\Serializable;
+use Lemuria\SerializableTrait;
+use Lemuria\Validate;
+
+class Fee implements Extension
+{
+	use BuilderTrait;
+	use ExtensionTrait;
+	use SerializableTrait;
+
+	private const FEE = 'fee';
+
+	protected Quantity|float|null $fee = null;
+
+	public function Fee(): Quantity|float|null {
+		return $this->fee;
+	}
+
+	public function serialize(): array {
+		$fee = $this->fee;
+		if ($this->fee instanceof Quantity) {
+			$fee = [(string)$this->fee->Commodity() => $this->fee->Count()];
+		}
+		return [self::FEE => $fee];
+	}
+
+	public function unserialize(array $data): Serializable {
+		$this->validateSerializedData($data);
+		$fee = $data[self::FEE];
+		if (is_array($fee)) {
+			$fee = new Quantity(self::createCommodity(key($fee)), current($fee));
+		}
+		$this->fee = $fee;
+		return $this;
+	}
+
+	public function setFee(Quantity|float|null $fee): Fee {
+		$this->fee = $fee;
+		return $this;
+	}
+
+	/**
+	 * @param array<string, mixed> $data
+	 */
+	protected function validateSerializedData(array $data): void {
+		if (!array_key_exists(self::FEE, $data)) {
+			throw new UnserializeException('Serialized data has no fee of type array or float.');
+		}
+		$fee = $data[self::FEE];
+		if (is_array($fee)) {
+			if (count($fee) !== 1 || !is_string(key($fee)) || !is_int(current($fee))) {
+				throw new UnserializeException('Serialized data has no fee of type Quantity array.');
+			}
+		} else {
+			$this->validate($data, self::FEE, Validate::FloatOrNull);
+		}
+	}
+}
--- src/Extension/Market.php
+++ src/Extension/Market.php
@@ -2,26 +2,19 @@
 declare(strict_types = 1);
 namespace Lemuria\Model\Fantasya\Extension;
 
-use Lemuria\Exception\UnserializeException;
-use Lemuria\Model\Fantasya\Extension;
 use Lemuria\Model\Fantasya\Factory\BuilderTrait;
 use Lemuria\Model\Fantasya\Market\Tradeables;
-use Lemuria\Model\Fantasya\Quantity;
 use Lemuria\Serializable;
 use Lemuria\SerializableTrait;
 use Lemuria\Validate;
 
-class Market implements Extension
+class Market extends Fee
 {
 	use BuilderTrait;
 	use ExtensionTrait;
 	use SerializableTrait;
 
-	private const FEE = 'fee';
-
 	private const TRADEABLES = 'tradeables';
-
-	protected Quantity|float|null $fee = null;
 
 	protected Tradeables $tradeables;
 
@@ -29,35 +22,19 @@ class Market implements Extension
 		$this->tradeables = new Tradeables();
 	}
 
-	public function Fee(): Quantity|float|null {
-		return $this->fee;
-	}
-
 	public function Tradeables(): Tradeables {
 		return $this->tradeables;
 	}
 
 	public function serialize(): array {
-		$fee = $this->fee;
-		if ($this->fee instanceof Quantity) {
-			$fee = [(string)$this->fee->Commodity() => $this->fee->Count()];
-		}
-		return [self::FEE => $fee, self::TRADEABLES => $this->tradeables->serialize()];
+		$data                    = parent::serialize();
+		$data[self::TRADEABLES] = $this->tradeables->serialize();
+		return $data;
 	}
 
 	public function unserialize(array $data): Serializable {
-		$this->validateSerializedData($data);
-		$fee = $data[self::FEE];
-		if (is_array($fee)) {
-			$fee = new Quantity(self::createCommodity(key($fee)), current($fee));
-		}
-		$this->fee = $fee;
+		parent::unserialize($data);
 		$this->tradeables->unserialize($data[self::TRADEABLES]);
-		return $this;
-	}
-
-	public function setFee(Quantity|float|null $fee): Market {
-		$this->fee = $fee;
 		return $this;
 	}
 
@@ -65,17 +42,7 @@ class Market implements Extension
 	 * @param array<string, mixed> $data
 	 */
 	protected function validateSerializedData(array $data): void {
-		if (!array_key_exists(self::FEE, $data)) {
-			throw new UnserializeException('Serialized data has no fee of type array or float.');
-		}
-		$fee = $data[self::FEE];
-		if (is_array($fee)) {
-			if (count($fee) !== 1 || !is_string(key($fee)) || !is_int(current($fee))) {
-				throw new UnserializeException('Serialized data has no fee of type Quantity array.');
-			}
-		} else {
-			$this->validate($data, self::FEE, Validate::FloatOrNull);
-		}
+		parent::validateSerializedData($data);
 		$this->validate($data, self::TRADEABLES, Validate::Array);
 	}
 }
