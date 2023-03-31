f69bdfd80f4942f30fc9aa86740c109b703f877b
lemuria-pbem <lemuria@online.de>
2023-03-31 19:28:22 +0200

    [TASK] Add unit mock object.

--- tests/Mock/UnitMock.php
+++ tests/Mock/UnitMock.php
@@ -2,12 +2,24 @@
 declare(strict_types = 1);
 namespace Lemuria\Tests\Model\Fantasya\Mock;
 
+use Lemuria\Id;
 use Lemuria\Model\Fantasya\Region;
 use Lemuria\Model\Fantasya\Unit;
 
 class UnitMock extends Unit
 {
+	private Id $id;
+
 	protected Region $region;
+
+	public function __construct(Id|int $id = 0) {
+		parent::__construct();
+		$this->id = is_int($id) ? new Id($id) : $id;
+	}
+
+	public function Id(): Id {
+		return $this->id;
+	}
 
 	public function Region(): Region {
 		return $this->region;
