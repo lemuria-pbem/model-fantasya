8c1755db57377e82e790a7f670421bbbecca2dbe
lemuria-pbem <lemuria@online.de>
2023-04-01 16:55:10 +0200

    [TASK] Add item set assertion for unit tests.

--- tests/ModelTest.php
+++ tests/ModelTest.php
@@ -2,6 +2,8 @@
 declare (strict_types = 1);
 namespace Lemuria\Tests\Model\Fantasya;
 
+use function Lemuria\getClass;
+use Lemuria\ItemSet;
 use Lemuria\Lemuria;
 use Lemuria\Model\Fantasya\SingletonCatalog;
 use Lemuria\Tests\Mock\Model\ConfigMock;
@@ -13,5 +15,20 @@ abstract class ModelTest extends Test
 		parent::setUpBeforeClass();
 		Lemuria::init(new ConfigMock());
 		Lemuria::Builder()->register(new SingletonCatalog());
+	}
+
+	/**
+	 * @param array<string, int> $expected
+	 */
+	public static function assertItemSet(array $expected, ItemSet $actual): void {
+		parent::assertSame(count($expected), $actual->count(), 'Item set count is different.');
+		foreach ($expected as $class => $count) {
+			parent::assertTrue($actual->offsetExists($class), 'Item set has no ' . getClass($class) . '.');
+			parent::assertSame($count, $actual[$class]->Count(), 'Item set does not have ' . $count . ' ' . getClass($class) . '.');
+		}
+		foreach ($actual as $item) {
+			$class = get_class($item->getObject());
+			parent::assertTrue(isset($expected[$class]), 'Item set has unexpected ' . $item . '.');
+		}
 	}
 }
