a554b0dd606c39016b72d9e17acbce01f12a176f
lemuria-pbem <lemuria@online.de>
2023-12-28 17:39:29 +0100

    [FEATURE] Add herbal hut.

---
+++ src/Building/HerbalHut.php
@@ -0,0 +1,59 @@
+<?php
+declare (strict_types = 1);
+namespace Lemuria\Model\Fantasya\Building;
+
+use Lemuria\Model\Fantasya\Building;
+use Lemuria\Model\Fantasya\BuildingEffect;
+use Lemuria\Model\Fantasya\Commodity\Iron;
+use Lemuria\Model\Fantasya\Commodity\Silver;
+use Lemuria\Model\Fantasya\Commodity\Stone;
+use Lemuria\Model\Fantasya\Commodity\Wood;
+use Lemuria\Model\Fantasya\Modification;
+use Lemuria\Model\Fantasya\Talent\Herballore;
+
+/**
+ * A cabin that improves woodchopping.
+ */
+final class HerbalHut extends AbstractBuilding
+{
+	private const int TALENT = 3;
+
+	private const int FEED = 5;
+
+	private const int SILVER = 100;
+
+	private const int WOOD = 5;
+
+	private const int STONE = 2;
+
+	private const int IRON = 1;
+
+	public function Dependency(): ?Building {
+		return Building::IS_INDEPENDENT;
+	}
+
+	public function Feed(): int {
+		return self::FEED;
+	}
+
+	public function Talent(): int {
+		return self::TALENT;
+	}
+
+	public function Upkeep(): int {
+		return Building::IS_FREE;
+	}
+
+	public function UsefulSize(): int {
+		return Building::IS_UNLIMITED;
+	}
+
+	protected function material(): array {
+		return [Silver::class => self::SILVER, Wood::class => self::WOOD, Stone::class => self::STONE, Iron::class => self::IRON];
+	}
+
+	protected function fill(BuildingEffect $buildingEffect): void {
+		$herballore = self::createTalent(Herballore::class);
+		$buildingEffect->add(new Modification($herballore, 1));
+	}
+}
