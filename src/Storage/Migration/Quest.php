6d1ff35e5eb97c144f99a88999385c97f97f96f3
lemuria-pbem <lemuria@online.de>
2024-02-05 07:52:11 +0100

    [TASK] Add migration for quests.

---
+++ src/Storage/Migration/Quest.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types = 1);
+namespace Lemuria\Model\Fantasya\Storage\Migration;
+
+class Quest extends AbstractModel
+{
+	public function __construct() {
+		$this->addInteger('id');
+		$this->addString('controller');
+		$this->addArray('payload');
+	}
+}
